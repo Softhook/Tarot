f627c5501b663ad83c46bde455598f39ec061ad0
Christian Nold <christian@softhook.com>
2025-09-09 13:06:24 +0100

    button locations on about screen

--- sketch.js
+++ sketch.js
@@ -228,7 +228,7 @@ function drawAboutScreen() {
   textAlign(LEFT, TOP);
   textSize(isMobile ? 16 : 18);
   textLeading(25);
-  let a = "This Design Wishes Tarot is composed of many people’s wishes and turns personal desires into a shared instrument. When you draw a card, you are invited to inhabit someone else’s longing and reflect on how it resonates with your own. \n\n The Deck was created in 2025 by Christian Nold, Georgy Holden, and James Warren as part of an Open University scholarship project. Over the last decade design students from the U101 module have been writing their wishes on postcards, which they posted back to the university. These messages were then transcribed and analysed, before being transformed into images using generative Artificial Intelligence and lots of human craft & judgement. The deck weaves together multiple layers: Tarot symbolism, the discipline of design, the visual language of generative AI, playful visual jokes, and the context of remote study at the Open University. The designs were produced using Adobe Firefly Image 3, Photoshop, p5.js, ChatGPT 01, and the typefaces Roman Holiday Sketch and Poppins and scans of the Rider-Waite Tarot.";
+  let a = "This Design Wishes Tarot is composed of many people’s wishes and turns personal desires into a shared instrument. When you draw a card, you are invited to inhabit someone else’s longing and reflect on how it resonates with your own. \n\n The Deck was created in 2025 by Christian Nold, Georgy Holden, and James Warren as part of an Open University scholarship project. Over the last decade design students from the U101 module have been writing their wishes on postcards, which they posted to the university. These messages were then transcribed and analysed, before being transformed into images using generative Artificial Intelligence and lots of human craft & judgement. The deck weaves together multiple layers: Tarot symbolism, the discipline of design, the visual language of generative AI, playful visual jokes, and the context of remote study at The Open University. The designs were produced using Adobe Firefly Image 3, Photoshop, p5.js, ChatGPT 01, and the typefaces Roman Holiday Sketch and Poppins and scans of the Rider-Waite Tarot.";
   text(a, width / 2, isMobile ? 50 : 80, 400);
   
   textAlign(CENTER, CENTER);
@@ -485,7 +485,7 @@ function mousePressed() {
   // Define toggle button boundaries
   let buttonW = 130;
   let buttonH = 40;
-  let toggleButtonY = (isMobile ? 650 : 720) - 60;
+  let toggleButtonY = (isMobile ? 820 : 890) - 60;
   let toggleButtonX1 = (width / 2) - buttonW / 2;
   let toggleButtonX2 = (width / 2) + buttonW / 2;
   let toggleButtonY1 = toggleButtonY - buttonH / 2;
@@ -498,7 +498,7 @@ function mousePressed() {
     showDescription = !showDescription;
   }
 
-    let backButtonY = isMobile ? 650 : 720;
+    let backButtonY = isMobile ? 820 : 890;
     let backButtonW = 100;
     let backButtonH = 40;
     let backButtonX1 = (width / 2) - backButtonW / 2;
